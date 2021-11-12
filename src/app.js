32dc33309f2f94620a2cfc154ec595f55c9f6041
esofronova <essofronova@gmail.com>
2021-11-12 13:09:28 -0800

    fixing height on small screens

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
 import './style.scss';
 
 export default function ChristmasCountdown() {
@@ -41,34 +42,36 @@ export default function ChristmasCountdown() {
    ];
 
    return (
-      <div className="christmas-countdown h-100 position-relative py-5 overflow-hidden centered bg-cover">
-         <div className="w-100">
-            <h1 className="header text-white text-uppercase">Christmas is coming</h1>
-            <div className="text-center text-white text-uppercase mt-5 ls-3 text-shadow">It's the most wonderful time of the year!</div>
-            <div className="d-flex flex-column flex-md-row justify-content-around align-items-center text-white text-center mt-5 mx-auto py-4 circles">
-               {data.map((item, index) => {
-                  return (
-                     <div key={index} className="circle centered rounded-circle border border-white border-5 mb-4 mb-md-0">
-                        <h1>{item.number}</h1>
-                        <h5>{item.value}</h5>
-                     </div>
-                  );
-               })}
+      <div className="christmas-countdown h-100 position-relative overflow-hidden">
+         <Container className="py-5 h-100 centered">
+            <div>
+               <h1 className="header text-white text-uppercase">Christmas is coming</h1>
+               <div className="text-center text-white text-uppercase mt-5 ls-3 text-shadow">It's the most wonderful time of the year!</div>
+               <div className="d-flex justify-content-around align-items-center mt-5 mx-auto py-4 circles">
+                  {data.map((item, index) => {
+                     return (
+                        <div key={index} className="circle centered rounded-circle border border-white border-3 text-white text-center ">
+                           <h1 className="mb-0">{item.number}</h1>
+                           <h5>{item.value}</h5>
+                        </div>
+                     );
+                  })}
+               </div>
+               <div className="text-center text-white text-uppercase mt-5 ls-3 text-shadow">"We wish you a Merry Christmas and a Happy New Year"</div>
             </div>
-            <div className="text-center text-white text-uppercase mt-5 ls-3 text-shadow">"We wish you a Merry Christmas and a Happy New Year"</div>
-            <div className="snow position-absolute">
-               {numberOfSnowflakes.map((index) => {
-                  let randomX = Math.floor(Math.random() * window.innerWidth);
-                  let randomY = Math.floor(Math.random() * window.innerHeight);
-                  return (
-                     <div
-                        key={index}
-                        className="snowflake rounded-circle position-absolute"
-                        style={{ left: randomX, top: randomY }}
-                     ></div>
-                  );
-               })}
-            </div>
+         </Container>
+         <div className="snow position-absolute w-100 h-100">
+            {numberOfSnowflakes.map((index) => {
+               let randomX = Math.floor(Math.random() * window.innerWidth);
+               let randomY = Math.floor(Math.random() * window.innerHeight);
+               return (
+                  <div
+                     key={index}
+                     className="snowflake rounded-circle position-absolute"
+                     style={{ left: randomX, top: randomY }}
+                  ></div>
+               );
+            })}
          </div>
       </div>
    );
